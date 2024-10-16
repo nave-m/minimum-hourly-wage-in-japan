10c10797dbbca9e486ae96856fa33f63970eefa9
Masato Watanabe <masato.nave@gmail.com>
2024-10-16 21:58:56 +0900

    feat: gRPCのアクセスログ出力

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ GRPC_TOOL=grpc_tools_node_protoc
 TYPESCRIPT_PLUGIN=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts
 WAGE_PROTO_PATH=./packages/grpc/doc/proto
 GOOGLE_PROTO_PATH=./node_modules/google-proto-files
-
-COMMAND=npx $(GRPC_TOOL) --plugin=${TYPESCRIPT_PLUGIN} --js_out=import_style=commonjs,binary:$(OUTPUT) --grpc_out=grpc_js:$(OUTPUT) --ts_out=grpc_js:$(OUTPUT) --proto_path=$(GOOGLE_PROTO_PATH) --proto_path=$(WAGE_PROTO_PATH) $(WAGE_PROTO_PATH)/jp/wage/api/v1/*.proto $(GOOGLE_PROTO_PATH)/google/type/date.proto $(GOOGLE_PROTO_PATH)/google/rpc/error_details.proto 
+DESCRIPTOR_OUTPUT=packages/grpc/src/gen/descriptor_set.bin
+COMMAND=npx $(GRPC_TOOL) --plugin=$(TYPESCRIPT_PLUGIN) --descriptor_set_out=$(DESCRIPTOR_OUTPUT) --include_imports --js_out=import_style=commonjs,binary:$(OUTPUT) --grpc_out=grpc_js:$(OUTPUT) --ts_out=grpc_js:$(OUTPUT) --proto_path=$(GOOGLE_PROTO_PATH) --proto_path=$(WAGE_PROTO_PATH) $(WAGE_PROTO_PATH)/jp/wage/api/v1/*.proto $(GOOGLE_PROTO_PATH)/google/type/date.proto $(GOOGLE_PROTO_PATH)/google/rpc/error_details.proto 
 
 .PHONY: protogen
 protogen:
--- packages/grpc/src/index.ts
+++ packages/grpc/src/index.ts
@@ -1,6 +1,9 @@
-import { Server, ServerCredentials} from "@grpc/grpc-js";
+import { Server, ServerCredentials, ServerInterceptingCall } from "@grpc/grpc-js";
 import { configure } from "./app";
 import { WinstonLoggingService } from '@minimum-hourly-wage-in-japan/winston/src/WinstonLoggingService';
+import { addReflection } from 'grpc-server-reflection'
+import { Message } from "google-protobuf";
+import { PartialStatusObject } from "@grpc/grpc-js/build/src/call-interface";
 
 const getPort = (mayBePortText: string | undefined): number => {
     const DEFAULT_PORT = 3000;
@@ -17,8 +20,55 @@ const getPort = (mayBePortText: string | undefined): number => {
 const port = getPort(process.env.GRPC_API_PORT);
 const host = `localhost:${port}`;
 
-const server = new Server(); // TODO: アクセスログを記録するInterceptorを注入する
 const loggingService = new WinstonLoggingService({service: 'minimum-hourly-wage-in-japan-grpc-api'})
+type AccessLoggingContext = {
+    request?: object;
+    response?: object;
+    status?: PartialStatusObject;
+};
+
+const server = new Server({
+    interceptors: [
+        (methodDescriptor, call) => {
+            const context: AccessLoggingContext = {};
+            return new ServerInterceptingCall(call, {
+                start: (next) => {
+                    next({
+                        onReceiveMetadata: (metadata, mdNext) => {
+                            mdNext(metadata);
+                        },
+                        onReceiveMessage: (message, messageNext) => {
+                            context.request = (message as Message).toObject();
+                            messageNext(message);
+                        },
+                        onReceiveHalfClose: (hcNext) => {
+                            hcNext();
+                        },
+                    });
+                },
+                sendMetadata: (metadata, mdNext) => {
+                    mdNext(metadata);
+                },
+                sendMessage: (message, messageNext) => {
+                    context.response = (message as Message).toObject();
+                    messageNext(message);
+                },
+                sendStatus: (status, statusNext) => {
+                    context.status = status;
+                    if (methodDescriptor.path != "/grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo") {
+                        loggingService.info({
+                            message: 'access-log',
+                            path: methodDescriptor.path,
+                            ...context,
+                        });
+                    }
+                    statusNext(status);
+                },
+            })
+        }
+    ],
+});
+addReflection(server, process.cwd() + '/src/gen/descriptor_set.bin');
 configure({
     server,
     loggingService
@@ -28,4 +78,4 @@ configure({
     } else {
         loggingService.info({message: `listening port: ${port}`});
     }
-})
+});
