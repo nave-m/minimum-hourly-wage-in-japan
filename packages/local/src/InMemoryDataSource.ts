582c273e4e0d73519106e0b7213dba30826fe51a
Masato Watanabe <masato.nave@gmail.com>
2025-09-22 09:42:15 +0900

    chore: 2025年9月22日の官報公示情報

--- packages/local/src/InMemoryDataSource.ts
+++ packages/local/src/InMemoryDataSource.ts
@@ -106,7 +106,7 @@ export class InMemoryDataSource {
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Aomori, hourlyWage: 1029, effectiveDate: LocalDate.fromISO8601('2025-11-21'), publicationDate: null}),
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Iwate, hourlyWage: 1031, effectiveDate: LocalDate.fromISO8601('2025-12-01'), publicationDate: null}),
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Miyagi, hourlyWage: 1038, effectiveDate: LocalDate.fromISO8601('2025-10-04'), publicationDate: LocalDate.fromISO8601('2025-09-04')}),
-            new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Akita, hourlyWage: 1031, effectiveDate: LocalDate.fromISO8601('2026-03-31'), publicationDate: null}),
+            new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Akita, hourlyWage: 1031, effectiveDate: LocalDate.fromISO8601('2026-03-31'), publicationDate: LocalDate.fromISO8601('2025-09-22')}),
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Yamagata, hourlyWage: 1032, effectiveDate: LocalDate.fromISO8601('2025-12-23'), publicationDate: null}),
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Fukushima, hourlyWage: 1033, effectiveDate: LocalDate.fromISO8601('2026-01-01'), publicationDate: null}),
             new MinimumHourlyWageRevision({prefectureCode: PrefectureCode.Ibaraki, hourlyWage: 1074, effectiveDate: LocalDate.fromISO8601('2025-10-12'), publicationDate: LocalDate.fromISO8601('2025-09-12')}),
